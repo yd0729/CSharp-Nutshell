610ff8f249e484cb46dd8fc8c8f2ff5440ca0f1d
杨东 <yangdong0729@outlook.com>
2022-07-21 22:35:49 +0800

    6.1

---
+++ 01. Char/Program.cs
@@ -0,0 +1,16 @@
+﻿// char literals:
+
+using System.Globalization;
+
+char c = 'A';
+char newLine = '\n';
+
+// System.Char defines a range of static methods for working with characters:
+Console.WriteLine(char.ToUpper('c')); // C
+Console.WriteLine(char.ToUpper('i', CultureInfo.InvariantCulture));  // 和地区无关
+Console.WriteLine(char.ToUpperInvariant('i'));
+
+Console.WriteLine(char.IsWhiteSpace('\t')); // True
+Console.WriteLine(char.IsLetter('x')); // True
+
+Console.WriteLine(char.GetUnicodeCategory('x')); // LowercaseLetter
---
+++ 05. Accessing Characters within a string/Program.cs
@@ -0,0 +1,5 @@
+﻿string str  = "abcde";
+char letter = str[1];        // letter == 'b'
+
+// string also implements IEnumerable<char>, so you can foreach over its characters:
+foreach (char c in "123") Console.Write (c + ",");    // 1,2,3,
