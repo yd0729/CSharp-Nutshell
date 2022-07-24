ce9bb10de90d0db4ce1258256cbe9f6164b853e6
杨东 <yangdong0729@outlook.com>
2022-07-24 20:24:56 +0800

    6.3.5 - 6.4.5

---
+++ Chapter 06. .NET Fundamentals/03. Dates and Time Zones/05. Daylight Saving and DateTime/Program.cs
@@ -0,0 +1,7 @@
+﻿// The IsDaylightSavingTime tells you whether a given local DateTime is subject to daylight saving.
+Console.WriteLine (DateTime.Now.IsDaylightSavingTime());     // True or False
+Console.WriteLine(DateTimeOffset.Now.LocalDateTime.IsDaylightSavingTime());
+
+// UTC times always return false: UTC 时间没有夏令时的概念
+Console.WriteLine (DateTime.UtcNow.IsDaylightSavingTime());  // Always False
+
---
+++ Chapter 06. .NET Fundamentals/04. Formatting and Parsing/01. ToString and Parse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+// The simplest formatting mechanism is the ToString method.
+string s = true.ToString();
+Console.WriteLine(s);
+
+// Parse does the reverse:
+bool b = bool.Parse(s);
+Console.WriteLine(b);
+
+// TryParse avoids a FormatException in case of error:
+int.TryParse("qwerty", out var i);
+Console.WriteLine(i);
+int.TryParse("123", out i);
+Console.WriteLine(i);
+
+if (int.TryParse("123", out int j))
+{
+    Console.WriteLine($"Use j: {j}");
+}
+
+bool validInt = int.TryParse("123", out int _);
+Console.WriteLine("We don't care about the actual value so use discard.");
+Console.WriteLine(validInt);
+
+// Culture trap:
+Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-DE"); // Germany
+Console.WriteLine("Parsing 1.234");
+Console.WriteLine(double.Parse("1.234")); // 1234 
+
+// Specifying invariant culture fixes this:
+Console.WriteLine("Parsing 1.234 Invariantly");
+Console.WriteLine(double.Parse("1.234", CultureInfo.InvariantCulture));
+
+Console.WriteLine("1.234.ToString()"); // Germany
+Console.WriteLine(1.234);
+Console.WriteLine("1.234.ToString Invariant");
+Console.WriteLine(1.234.ToString(CultureInfo.InvariantCulture));
